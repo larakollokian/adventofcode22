d0f06b7fa27b476fda8e1c5500bc4d86ef1fc745
Lara Kollokian <lara.kollokian@gmail.com>
2022-12-03 03:02:47 -0500

    day 2 part 2

--- day2/rock_paper_scissors.rb
+++ day2/rock_paper_scissors.rb
@@ -1,3 +1,35 @@
+A = {
+    win: "B",
+    draw: "A",
+    lose: "C"
+}
+
+B = {
+    win: "C",
+    draw: "B",
+    lose: "A"
+}
+
+C = {
+    win: "A",
+    draw: "C",
+    lose: "B"
+}
+
+def get_points(hand)
+    case hand
+    when "X"
+    when "A"
+        1
+    when "Y"
+    when "B"
+        2
+    when "Z"
+    when "C"
+        3
+    end
+end
+
 def part_one
     score = 0
     File.readlines('day2/input.txt').each do |line|
@@ -12,7 +44,7 @@ def part_two
     score = 0
     File.readlines('day2/input.txt').each do |line|
         game = line.split(" ")
-        score = score + calculate_score(game.first, game.last)
+        score = score + calculate_score_2(game.first, game.last)
     end
     
     puts score
@@ -47,15 +79,36 @@ def calculate_score(left_hand, right_hand)
     end
 end
 
-def get_points(hand)
-    case hand
-    when "X"
-        1
-    when "Y"
-        2
-    when "Z"
-        3
+def calculate_score_2(left_hand, move)
+    case left_hand
+    when "A"
+        if move == "X" then
+            get_points(A[:lose])
+        elsif move == "Y" then
+            get_points(A[:draw]) + 3
+        else
+            get_points(A[:win]) + 6
+        end 
+    when "B"
+        if move == "X" then
+            get_points(B[:lose])
+        elsif move == "Y" then
+            get_points(B[:draw]) + 3
+        else
+            get_points(B[:win]) + 6
+        end 
+    when "C"
+        if move == "X" then
+            get_points(C[:lose])
+        elsif move == "Y" then
+            get_points(C[:draw]) + 3
+        else
+            get_points(C[:win]) + 6
+        end 
     end
 end
 
+puts "Part one: "
 part_one
+puts "\nPart two: "
+part_two
