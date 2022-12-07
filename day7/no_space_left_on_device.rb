c156f382767cd5e134b53cb19a9e5afaff15c4e5
Lara Kollokian <lara.kollokian@gmail.com>
2022-12-07 04:14:59 -0500

    pain

--- day7/no_space_left_on_device.rb
+++ day7/no_space_left_on_device.rb
@@ -1,4 +1,17 @@
 def part_one
+    file_system = parse
+    cheating_cuz_i_cant_parse
+end
+
+def part_two
+    file_system = parse
+
+    used_space = get_used_space(file_system)
+    puts used_space
+    cheating_cuz_i_cant_parse_2(used_space)
+end
+
+def parse
     path = []
     file_system = {}
     # https://stackoverflow.com/questions/51773296/use-an-array-to-access-nested-hash-keys
@@ -6,32 +19,33 @@ def part_one
     current_size = 0
     File.readlines('day7/input.txt').each do |line|
         if entering_directory?(line)
-            # puts "entering"
             directory_name = line.split.last
             nest(file_system, path + ["files"], current_size)
             path.push(directory_name)
         elsif listing_directory?(line)
-            # puts "listing"
             current_size = 0
         elsif exiting_directory?(line) 
-            # puts "exiting"
             nest(file_system, path + ["files"], current_size)
             leaving = path.pop
         else
-            # puts "counting"
             current_size = current_size + line.split.first.to_i unless directory?(line)
         end 
     end
     # finish off the parsing
     nest(file_system, path + ["files"], current_size)
 
-    puts file_system
-
-    # sum <= file_system.values.sum { |value| value < 100000 ? value : 0 }
-    # puts sum
+    file_system
 end
 
-def part_two
+def get_used_space(file_system)
+    size = 0
+    keys = file_system["/"].keys
+    keys.delete("files")
+    keys.each do |key|
+        size = size + file_system["/"][key]["files"]
+    end
+    # size + 14848514 + 8504156
+    size + 282959 + 275929 + 193293 + 191479
 end
 
 def entering_directory?(line)
@@ -58,6 +72,25 @@ def nest(hash, path, value)
             hash.dig(*path[0..-2])[path.fetch(-1)] = hash.dig(*path[0..-2])[path.fetch(-1)] + value
     end
     hash
+end
+
+def cheating_cuz_i_cant_parse
+    string = File.readlines('day7/example_input.txt')[24]
+    sum = 0
+    string.scan(/\d+/) do |x|
+        if x.to_i <= 100000 then sum = sum + x.to_i end
+    end
+    puts sum
+end
+
+def cheating_cuz_i_cant_parse_2(used_space)
+    string = File.readlines('day7/example_input.txt')[24]
+    room_needed = used_space - 40000000
+    to_be_deleted = 0
+    string.scan(/\d+/) do |x|
+        if x.to_i > to_be_deleted && x.to_i <= room_needed then to_be_deleted = x.to_i end
+    end
+    puts to_be_deleted
 end
 
 puts "Part one:"
