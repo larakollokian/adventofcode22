3dd3dc4a800af4e02b7d4290ab60fb6c425ac0ef
Lara Kollokian <lara.kollokian@gmail.com>
2022-12-07 02:59:15 -0500

    idk how to sum the sizes...

--- day7/no_space_left_on_device.rb
+++ day7/no_space_left_on_device.rb
@@ -4,17 +4,21 @@ def part_one
     # https://stackoverflow.com/questions/51773296/use-an-array-to-access-nested-hash-keys
     file_system.default_proc = -> (h, k) { h[k] = Hash.new(&h.default_proc) }
     current_size = 0
-    File.readlines('day7/example_input.txt').each do |line|
+    File.readlines('day7/input.txt').each do |line|
         if entering_directory?(line)
+            # puts "entering"
             directory_name = line.split.last
             nest(file_system, path + ["files"], current_size)
             path.push(directory_name)
         elsif listing_directory?(line)
+            # puts "listing"
             current_size = 0
         elsif exiting_directory?(line) 
+            # puts "exiting"
             nest(file_system, path + ["files"], current_size)
             leaving = path.pop
         else
+            # puts "counting"
             current_size = current_size + line.split.first.to_i unless directory?(line)
         end 
     end
@@ -22,6 +26,9 @@ def part_one
     nest(file_system, path + ["files"], current_size)
 
     puts file_system
+
+    # sum <= file_system.values.sum { |value| value < 100000 ? value : 0 }
+    # puts sum
 end
 
 def part_two
@@ -36,7 +43,7 @@ def listing_directory?(line)
 end
 
 def exiting_directory?(line)
-    /\$ cd ../.match(line)
+    /\$ cd \.\./.match(line)
 end
 
 def directory?(line)
@@ -45,8 +52,10 @@ end
 
 # https://stackoverflow.com/questions/51773296/use-an-array-to-access-nested-hash-keys
 def nest(hash, path, value)
-    if path[0] != "files" && hash.dig(*path[0..-2])[path.fetch(-1)] == {}
-        hash.dig(*path[0..-2])[path.fetch(-1)] = value
+    if path[0] != "files"
+        hash.dig(*path[0..-2])[path.fetch(-1)] == {} ? 
+            hash.dig(*path[0..-2])[path.fetch(-1)] = value : 
+            hash.dig(*path[0..-2])[path.fetch(-1)] = hash.dig(*path[0..-2])[path.fetch(-1)] + value
     end
     hash
 end
